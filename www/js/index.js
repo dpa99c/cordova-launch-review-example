590d9452db10f245bd398bb2043278aa5216918e
Dave Alden <dpa99c@gmail.com>
2020-10-30 07:16:09 +0000

    (Android): Fix logic for in-app rating dialog.

--- www/js/index.js
+++ www/js/index.js
@@ -1,6 +1,7 @@
 var platform, ratingTimerId;
 
-var MAX_DIALOG_WAIT_TIME = 5000; //max time to wait for rating to display
+var MAX_DIALOG_WAIT_TIME_IOS = 5*1000; //max time to wait for rating dialog to display on iOS
+var MAX_DIALOG_WAIT_TIME_ANDROID = 60*1000; //max time to wait for rating dialog to display on Android and be submitted by user
 
 function init(){
     platform = navigator.userAgent.match('Android') ? "android" : "ios";
@@ -10,7 +11,16 @@ function init(){
     $('#isRatingSupported').on("click", isRatingSupported);
 }
 
+function clearRatingDialogTimer(){
+    if(ratingTimerId){
+        clearTimeout(ratingTimerId);
+        ratingTimerId = null;
+    }
+}
+
 function showAlert(msg){
+    clearRatingDialogTimer();
+    hidePreloader();
     navigator.notification.alert(msg);
 }
 
@@ -23,21 +33,24 @@ function launchreview(){
 }
 
 function launchrating(){
+    if(cordova.platformId === "android"){
+        ratingTimerId = setTimeout(ratingDialogNotShown, MAX_DIALOG_WAIT_TIME_ANDROID);
+    }
+
+    // Request the rating dialog
     LaunchReview.rating(function(status){
-        if(cordova.platformId === "android"){
-            showAlert("Rating dialog displayed");
-        }else if(cordova.platformId === "ios"){
-            if(status === "requested"){
+        if(status === "requested"){
+            if(cordova.platformId === "android"){
+                showAlert("Displayed rating dialog");
+            }else if(cordova.platformId === "ios"){
                 console.log("Requested rating dialog");
-                ratingTimerId = setTimeout(ratingDialogNotShown, MAX_DIALOG_WAIT_TIME);
+                ratingTimerId = setTimeout(ratingDialogNotShown, MAX_DIALOG_WAIT_TIME_IOS);
                 showPreloader();
-            }else if(status === "shown"){
-                clearTimeout(ratingTimerId);
-                hidePreloader();
-                showAlert("Rating dialog displayed");
-            }else if(status === "dismissed"){
-                showAlert("Rating dialog dismissed");
             }
+        }else if(status === "shown"){
+            showAlert("Rating dialog displayed");
+        }else if(status === "dismissed"){
+            showAlert("Rating dialog dismissed");
         }
     }, function (err){
         showAlert("Error launching rating dialog: " + err);
@@ -50,8 +63,12 @@ function isRatingSupported(){
 }
 
 function ratingDialogNotShown(){
-    hidePreloader();
-    var msg = "Rating dialog was not shown (after " + MAX_DIALOG_WAIT_TIME + "ms)";
+    var msg;
+    if(cordova.platformId === "android"){
+        msg = "Rating dialog outcome not received (after " + MAX_DIALOG_WAIT_TIME_ANDROID + "ms)";
+    }else if(cordova.platformId === "ios"){
+        msg = "Rating dialog was not shown (after " + MAX_DIALOG_WAIT_TIME_IOS + "ms)";
+    }
     console.warn(msg);
     showAlert(msg);
 }
@@ -63,6 +80,5 @@ function showPreloader(){
 function hidePreloader(){
     $('#preloader').hide();
 }
-
 
 $(document).on("deviceready", init);
